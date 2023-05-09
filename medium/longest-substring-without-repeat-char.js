000f4dc5d5f3290d3f80b3ed8fc057c34a24fa0c
Sinz <53941883+sinz134@users.noreply.github.com>
2023-05-09 15:58:34 +0700

    Add files via upload

---
+++ medium/longest-substring-without-repeat-char.js
@@ -0,0 +1,35 @@
+// Given a string s, find the length of the longest
+// substring
+// without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+    let max_len = 0;
+    let curr = 0;
+    let hash = {}; 
+    if(s.length < 2) {
+        return s.length;
+    }
+    for(let i = 0; i < s.length;  i++) {
+        if(hash[s[i]] == null) {
+            curr += 1;
+        } else {
+            curr = Math.min(i - hash[s[i]], curr + 1);
+        }
+        max_len = Math.max(max_len, curr);
+        hash[s[i]] = i; //save the index
+    }
+    return max_len;
+};
