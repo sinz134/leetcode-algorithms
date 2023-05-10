4863ad6bf5a8340093cf9b3af0cc27dabfa62641
Sinz <53941883+sinz134@users.noreply.github.com>
2023-05-10 14:37:12 +0700

    Add files via upload

---
+++ easy/merge-two-sorted-lists.js
@@ -0,0 +1,29 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
